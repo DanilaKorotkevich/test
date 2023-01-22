5b64025c23aa8982055035992303ee83bf5d5439
Danila <danilakorotkevich62@gmail.com>
2023-01-22 12:04:13 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class Main {
+    public static void main(String[] args) {
+        int age,year,birth;
+        String result;
+        result = JOptionPane.showInputDialog("Какой сейчас год?");
+        year = Integer.parseInt(result);
+        result = JOptionPane.showInputDialog("Сколько вам лет?");
+        age = Integer.parseInt(result);
+        birth = year - age - 1;
+        JOptionPane.showMessageDialog(null, "Вы родились в " + birth + " году");
+    }
+}
